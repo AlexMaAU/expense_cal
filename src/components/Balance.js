11c60e0fe0c1d7c981a8add0aba28330e0190f7d
AlexMaAU <alexma8809@gmail.com>
2023-07-07 13:12:44 +1000

    styling adjusted

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react"
 import {GlobalContext} from '../context/GlobalState'
 
 export const Balance = ()=>{
+    // 使用React.createContext创建一个上下文对象，并使用Provider组件将共享的数据传递给后代组件。
+    // 然后可以在子组件中使用useContext钩子获取context的值 - 针对需要使用useContext的值进行逻辑运算
     const {transactions} = useContext(GlobalContext)
     const [balance, setBalance] = useState(0)
 
@@ -17,6 +19,10 @@ export const Balance = ()=>{
         <>
             <h4>Your Balance</h4>
             <h2 id='balance'>${balance}</h2>
+            {/* 也可以通过 context consumer 消费Provider中的value - 适用于直接渲染context的值 */}
+            {/* <GlobalContext.Consumer>
+                {value=><div>{value.transactions}</div>}
+            </GlobalContext.Consumer> */}
         </>
     )
 }
